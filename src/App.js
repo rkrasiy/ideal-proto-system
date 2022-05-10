e477187f23c5d232d6d00f0ffc7040f24b79a04b
Ruslan <krasiyruslan@gmail.com>
2022-05-10 23:02:02 +0200

    start-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {ThemeContext, themes} from './context/theme-context';
+import ThemedButton from './components/themed-button';
 
-function App() {
+// Промежуточный компонент, который использует ThemedButton
+function Toolbar(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemedButton onClick={props.changeTheme}>
+      Change Theme
+    </ThemedButton>
   );
 }
 
-export default App;
+export default class App2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: themes.light,
+    };
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme:
+          state.theme === themes.dark
+            ? themes.light
+            : themes.dark,
+      }));
+    };
+  }
+
+  render() {
+    // ThemedButton внутри ThemeProvider использует
+    // значение светлой UI-темы из состояния, в то время как
+    // ThemedButton, который находится вне ThemeProvider,
+    // использует тёмную UI-тему из значения по умолчанию
+    return (
+      <main>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+        </ThemeContext.Provider>
+        <section>
+          <ThemedButton />
+        </section>
+      </main>
+    );
+  }
+}
---
+++ src/components/theme-context.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import {ThemeContext} from './../context/theme-context';
+
+class ThemedButton extends React.Component {
+  render() {
+    let props = this.props;
+    let theme = this.context;
+    return (
+      <button
+        {...props}
+        style={{backgroundColor: theme.background}}
+      />
+    );
+  }
+}
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
